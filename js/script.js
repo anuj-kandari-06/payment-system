5e275da6130d8e4696284509f3446e946e7ef1b8
Anuj <anujkandari623@gmail.com>
2026-09-10 18:52:40 +0530

    first commit

---
+++ js/script.js
@@ -0,0 +1,34 @@
+const addBtn = document.getElementById("addBtn");
+const buyBtn = document.getElementById("buyBtn");
+const payBtn = document.getElementById("payBtn");
+const message = document.getElementById("message");
+
+addBtn.addEventListener("click", function () {
+    addBtn.innerHTML = "Adding..";
+    message.innerHTML = "Adding product to cart..";
+    setTimeout(function () {
+        addBtn.classList.add("hidden");
+        buyBtn.classList.remove("hidden");
+        message.innerHTML = "Product add to cart successfully";
+    }, 2000);
+});
+
+buyBtn.addEventListener("click", function () {
+    buyBtn.innerHTML = "Processing..";
+    message.innerHTML = "Processing your order..";
+    setTimeout(function () {
+        buyBtn.classList.add("hidden");
+        payBtn.classList.remove("hidden");
+        message.innerHTML = "Your order is reday, please make payment..";
+    }, 2000);
+});
+
+payBtn.addEventListener("click", function () {
+    payBtn.innerHTML = "wating..";
+    payBtn.disabled = true;
+    message.innerHTML = "Processing payment..";
+    setTimeout(function () {
+        payBtn.innerHTML = "Pay Now";
+        message.innerHTML = "Payment successfully completed..";
+    }, 3000);
+});
